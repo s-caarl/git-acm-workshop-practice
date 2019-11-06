58753cc3f0bd9e8b0827dfc63747f6d0e3b9d38b
s-caarl <cantado@nevada.unr.edu>
2019-11-05 20:06:11 -0800

    Created outline for 4-function integer calculator

---
+++ calculator.c
@@ -0,0 +1,34 @@
+//4 function integer calculator
+
+#include <stdio.h>
+
+int addition(x, y);
+int subtraction(x, y);
+int multiplication(x, y);
+int division(x,y);
+
+int main()
+{
+	return 0;
+}
+
+int addition(x, y)
+{
+	return 0;
+}
+
+int subtraction(x, y)
+{
+	return 0;
+}
+
+int multiplication(x, y)
+{
+	return 0;
+}
+
+int division(x,y)
+{
+	return 0;
+}
+
