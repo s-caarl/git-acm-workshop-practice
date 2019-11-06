da025a275c8a16ee35f1f9c87f59a1e4d8126786
s-caarl <cantado@nevada.unr.edu>
2019-11-05 20:17:31 -0800

    Created main program

--- calculator.c
+++ calculator.c
@@ -9,6 +9,53 @@ int division(x,y);
 
 int main()
 {
+	int a, b, menu;
+	do
+	{
+		printf("0. EXIT\n");
+		printf("1. Addition\n");
+		printf("2. Subtraction\n");
+		printf("3. Multiplication\n");
+		printf("4. Division\n");
+		scanf("%d", &menu);
+
+		switch (menu)
+		{
+			case 1:
+				printf("Enter integer 1: ");
+				scanf("%d", &a);
+				prinft("Enter integer 2: ");
+				scanf("%d", &b);
+				printf("SUM: %d\n", addition(a, b);
+				break;
+			case 2:
+				printf("Enter integer 1: ");
+				scanf("%d", &a);
+				prinft("Enter integer 2: ");
+				scanf("%d", &b);
+				printf("DIFFERENCE: %d\n", subtraction(a, b);
+				break;
+			case 3:
+				printf("Enter integer 1: ");
+				scanf("%d", &a);
+				prinft("Enter integer 2: ");
+				scanf("%d", &b);
+				printf("PRODUCT: %d\n", multiplication(a, b);
+				break;
+			case 4:
+				printf("Enter integer 1: ");
+				scanf("%d", &a);
+				prinft("Enter integer 2: ");
+				scanf("%d", &b);
+				printf("QUOTIENT: %d\n", division(a, b);
+				break;
+			case 0:
+				menu = 0;
+				break;
+		}
+	}
+	while (menu != 0);
+
 	return 0;
 }
 
