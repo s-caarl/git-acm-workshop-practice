86f4207910311d3492fe5fef2edd643835b10771
s-caarl <cantado@nevada.unr.edu>
2019-11-05 19:46:37 -0800

    Added main function

--- main.c
+++ main.c
@@ -7,6 +7,20 @@ float math_function(float x)
 
 int main()
 {
-	
+	int boundA, boundB;
+	float dx;
+
+	printf("Enter your bounds (a, b): ");
+	scanf("%d, %d", &boundA, &boundB);
+	printf("Enter your dx: ");
+	scanf("%f", &dx);
+
+	float integral = 0;
+	for (float x = boundA; x < boundB; x += dx)
+	{
+		integral += math_function(x) * dx;
+	}
+
+	printf("Integral between %d and %d with dx of %f is: %f\n", boundA, boundB, dx, integral);
 	return 0;
 }
